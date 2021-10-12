a8fe75c4c92a412ee85493e69e2991a1f7f01f33
Master <85671955+abhimohan-tech@users.noreply.github.com>
2021-10-12 13:02:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,7 @@ async def filter(client, message):
         else:
             await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADZQQAAn-26FfSFJs_uVTHfAI')
             
-        else:
+        
             await asyncio.sleep(20) 
             await client.send_message(chat_id=message.from_user.id,message="Check Your Spelling", Show_alert=True)
         if not btn:
