c14ebbfd8a6c7ac033ca125e8f1575446d683489
Master <85671955+abhimohan-tech@users.noreply.github.com>
2021-10-14 13:14:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -133,21 +133,26 @@ async def group(client, message):
             await client.send_message(
             chat_id = message.chat.id,
 
-            text=f"""(
+            text=f"""
 
 👋Hey Bruh
 
-If this movie is not in our database you will not get that movie..
 
-Otherwise, the spelling of the name of the requested movie may not be correct...
+Sorry, No Movie/Series Related to the Given Word Was Found 🥺
 
-So you go to google and check the spelling of the name of the movie you want.
+Please Go to Google and Confirm the Correct Spelling 🙏"""
 
-ഈ സിനിമ ഞങ്ങളുടെ ഡാറ്റാബേസിൽ ഇല്ലെങ്കിൽ നിങ്ങൾക്ക് ഈ സിനിമ ലഭിക്കില്ല
 
-അല്ലെങ്കിൽ, <b>അഭ്യർത്ഥിച്ച സിനിമയുടെ പേരിന്റെ അക്ഷരവിന്യാസം ശരിയായിരിക്കില്ല ...
 
-അതിനാൽ നിങ്ങൾ ഗൂഗിളിൽ പോയി നിങ്ങൾക്ക് ആവശ്യമുള്ള സിനിമയുടെ പേരിന്റെ സ്പെല്ലിംഗ് പരിശോധിക്കുക</b>""", del_in = 5)
+
+
+
+
+
+
+
+
+
 
             parse_mode="html",
 
