0a31c5415c13e5b46a141f05b4a230c8498cde14
Master <85671955+abhimohan-tech@users.noreply.github.com>
2021-10-16 09:43:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,6 @@ async def filter(client, message):
         else:
             await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADZQQAAn-26FfSFJs_uVTHfAI')
             return
-
         if not btn:
             return
             
